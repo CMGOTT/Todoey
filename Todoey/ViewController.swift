2d94ec342a41db97c4bde3fa9fa66d1c9bc8070e
CMG <cmgott@swbell.net>
2019-07-11 16:12:17 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by CMG on 7/11/19.
+//  Copyright © 2019 CMG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
